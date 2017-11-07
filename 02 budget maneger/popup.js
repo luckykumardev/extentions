d62e66bd46a88d18a36fb4804d5cc680a54f035a
lucky <lucky>
2017-11-07 10:06:10 -0800

    first commit

---
+++ 02 budget maneger/popup.js
@@ -0,0 +1,26 @@
+//////////////
+$(function(){
+
+    chrome.storage.sync.get(['total','limit'],function(budget){
+        $('#total').text(budget.total);
+    });
+
+    $('#spendAmount').click(function(){
+        chrome.storage.sync.get(['total', 'limit'],function(budget){
+            var newTotal = 0;
+            if (budget.total){
+                newTotal += parseInt(budget.total);
+            }
+
+            var amount = $('#amount').val();
+            if (amount){
+                newTotal += parseInt(amount);
+            }
+
+            chrome.storage.sync.set({'total': newTotal});
+            $('#total').text(newTotal);
+            $('#amount').val('');
+
+        });
+    });
+});
